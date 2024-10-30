417d086a19d4dfd185d2902dda8b07c4aadbe6ca
Sneha-janapurkar <janapurkarsneha028@gmail.com>
2024-10-30 23:38:13 +0530

    Add files via upload

---
+++ Mainflow task 1 (1).py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Creating a list
+my_list = [1, 2, 3, 4, 5]
+print("Original List:", my_list)
+
+# Adding an element to the list
+my_list.append(6)
+print("List after adding an element:", my_list)
+
+# Removing an element from the list
+my_list.remove(3)
+print("List after removing an element:", my_list)
+
+# Modifying an element in the list
+my_list[2] = 10
+print("List after modifying an element:", my_list)
+
+# Creating a dictionary
+my_dict = {"name": "Alice", "age": 25, "city": "New York"}
+print("\nOriginal Dictionary:", my_dict)
+
+# Adding a key-value pair to the dictionary
+my_dict["profession"] = "Engineer"
+print("Dictionary after adding a key-value pair:", my_dict)
+
+# Removing a key-value pair from the dictionary
+del my_dict["age"]
+print("Dictionary after removing a key-value pair:", my_dict)
+
+# Modifying a value in the dictionary
+my_dict["city"] = "Los Angeles"
+print("Dictionary after modifying a value:", my_dict)
+
+# Creating a set
+my_set = {1, 2, 3, 4, 5}
+print("\nOriginal Set:", my_set)
+
+# Adding an element to the set
+my_set.add(6)
+print("Set after adding an element:", my_set)
+
+# Removing an element from the set
+my_set.remove(2)
+print("Set after removing an element:", my_set)
+
+# Trying to add a duplicate element (set won't change as it doesn’t allow duplicates)
+my_set.add(3)
+print("Set after attempting to add a duplicate element:", my_set)
+
+
+# In[2]:
+
+
+# Creating a list
+my_list = [1, 2, 3, 4, 5]
+print("Original List:", my_list)
+
+# Adding an element to the list
+my_list.append(6)
+print("List after adding an element:", my_list)
+
+# Removing an element from the list
+my_list.remove(3)
+print("List after removing an element:", my_list)
+
+# Modifying an element in the list
+my_list[2] = 10
+print("List after modifying an element:", my_list)
+
+
+# In[3]:
+
+
+# Creating a dictionary
+my_dict = {"name": "Alice", "age": 25, "city": "New York"}
+print("\nOriginal Dictionary:", my_dict)
+
+# Adding a key-value pair to the dictionary
+my_dict["profession"] = "Engineer"
+print("Dictionary after adding a key-value pair:", my_dict)
+
+# Removing a key-value pair from the dictionary
+del my_dict["age"]
+print("Dictionary after removing a key-value pair:", my_dict)
+
+# Modifying a value in the dictionary
+my_dict["city"] = "Los Angeles"
+print("Dictionary after modifying a value:", my_dict)
+
+
+# In[4]:
+
+
+# Creating a set
+my_set = {1, 2, 3, 4, 5}
+print("\nOriginal Set:", my_set)
+
+# Adding an element to the set
+my_set.add(6)
+print("Set after adding an element:", my_set)
+
+# Removing an element from the set
+my_set.remove(2)
+print("Set after removing an element:", my_set)
+
+# Trying to add a duplicate element (set won't change as it doesn’t allow duplicates)
+my_set.add(3)
+print("Set after attempting to add a duplicate element:", my_set)
+
+
+# In[ ]:
+
+
+
+
